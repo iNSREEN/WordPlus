50d31fb3a12893cc9d52bd196ce3661b4468e127
lulu <lulukalmisfer@hotmail.com>
2023-01-09 15:56:09 +0300

    Change text test

--- WordPlus/ContentView.swift
+++ WordPlus/ContentView.swift
@@ -10,10 +10,8 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+         
+            Text("Hello, meow!")
         }
         .padding()
     }
