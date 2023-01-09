259c769ede06f85d04fc4f36005ac28754a284bc
Nsreen Alfaifi <nmalfaifi01@gmail.com>
2023-01-09 15:27:33 +0300

    Initial Commit

---
+++ WordPlus/WordPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordPlusApp.swift
+//  WordPlus
+//
+//  Created by Nsreen Alfaifi on 16/06/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct WordPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
